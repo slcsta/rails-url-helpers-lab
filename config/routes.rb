22908572a33d241e2428c7b675543e2f5e2a034f
Sarah <sarahlchavez@gmail.com>
2021-06-27 19:28:57 -0700

    commit tree be3cb492f406018de1aa7d0d628b27a22f29f80c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # A user can navigate to /students to view all of the students
+  # Route to view has an index page
+  # Should have individual students' show page
+
+  resources :students, only: [:index, :show] #, as:'students'
+
+  get 'students/:id/activate', to: 'students#activate', as:'activate-student'
 end
